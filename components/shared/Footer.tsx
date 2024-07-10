e98bda0be0896d8a00d530a43343eb1f30f253a9
danielasakpa <danielasakpa@gmail.com>
2024-07-09 18:03:55 -0700

    refactor: Improve Footer layout for better responsiveness
    - Updated the Footer component to use flexbox for better column and row alignment.
    - Added gap and margin adjustments for better spacing between links.
    - Ensured links are displayed in a column layout on smaller screens and in a row layout on larger screens

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -3,20 +3,37 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="p-16 flex items-center justify-center lg:rounded-md bg-foreground">
-      <span className="p-regular-18 text2 text-white">
+      <span className="flex flex-col lg:flex-row gap-2 lg:gap-4 p-regular-18 text2 text-white">
         {" "}
         © 2024 - InstaBucks and Daniel Asakpa, Inc.{" "}
-        <a href="" target="_blank" rel="noopener noreferrer" className="text-[#D10363]">
-          Privacy Policy
-        </a>{" "}
-        |{" "}
-        <a href="" target="_blank" rel="noopener noreferrer" className="text-[#D10363]">
-          Terms & Conditions{" "}
-        </a>{" "}
-        |{" "}
-        <a href="" target="_blank" rel="noopener noreferrer" className="text-[#D10363]">
-          Become an Affiliate!
-        </a>
+        <div className="flex flex-col lg:flex-row gap-2 mt-4 lg:mt-0">
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#D10363]"
+          >
+            Privacy Policy
+          </a>
+          <span className="hidden lg:block">|</span>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#D10363]"
+          >
+            Terms & Conditions{" "}
+          </a>{" "}
+          <span className="hidden lg:block">|</span>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#D10363]"
+          >
+            Become an Affiliate!
+          </a>
+        </div>
       </span>
     </div>
   );
